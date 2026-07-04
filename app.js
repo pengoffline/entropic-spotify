1ab7037fc8510ed8b936add1720524a954d9bc50
penguin offline <peng.offline@gmail.com>
2026-07-04 22:58:39 +0800

    Fix comment formatting in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ========================================
 // 基本設定
-// ========================================
+// ======================================== 
 const clientId = "18a7af28818a40abafa707e98e4d7a48"; // 換成你自己的
 const redirectUri = "https://pengoffline.github.io/entropic-spotify/index.html"; // 要跟Dashboard設定的一致
 // 需要的權限:個人資料 + 最近聆聽紀錄
